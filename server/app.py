9644648d28a50f3e69488c944e57657ba44085dc
IanNjeru17 <iannjeru17@gmail.com>
2024-10-29 17:08:00 +0300

    app

--- server/app.py
+++ server/app.py
@@ -34,6 +34,7 @@ def create_app():
 
     return app 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5555))
+    app.run(host="0.0.0.0", port=port, debug=True)
